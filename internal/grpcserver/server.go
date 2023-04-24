8938c14b10eee94c479903d926eba55df0e5342c
Эдгар Сипки <esipki@tsum.ru>
2023-04-24 17:16:33 +0000

    feat(DELTA-700): add configs to ListOffersConfig

--- internal/grpcserver/server.go
+++ internal/grpcserver/server.go
@@ -14,7 +14,8 @@ import (
 )
 
 const (
-	fieldOfferStatus = "offer.status"
+	fieldOfferStatus   = "offer.status"
+	fieldOfferSellerID = "offer.seller_id"
 )
 
 type server struct {
@@ -109,6 +110,9 @@ func (s server) ListOffers(ctx context.Context, request *offer_read_service.List
 
 func buildListOffersRepoRequest(request offer_read_service.ListOffersRequest) v1.GetListRequest {
 	listRequest := *request.Data
+	if listRequest.Sort != nil {
+		return listRequest
+	}
 
 	offerStatusFilterValue := getOfferStatusFilterValue(request.Data.Filters)
 	if offerStatusFilterValue != "" {
@@ -128,17 +132,19 @@ func getOfferStatusFilterValue(filters *v1.GetListRequest_FilterGroup) string {
 
 	for _, filter := range filters.Filters {
 		if filter.Field == "offer.status" {
-			switch {
-			case lo.Contains(filter.GetFilterTextIn().Value, string(model.OfferStatusCodeNew)):
-				return "new"
-			case lo.Contains(filter.GetFilterTextIn().Value, string(model.OfferStatusCodeSales)):
-				return "sales"
-			case lo.Contains(filter.GetFilterTextIn().Value, string(model.OfferStatusCodeInOrder)):
-				return "order"
-			case lo.Contains(filter.GetFilterTextIn().Value, string(model.OfferStatusCodeSold)):
-				return "sold"
-			case lo.Contains(filter.GetFilterTextIn().Value, string(model.OfferStatusCodeReturnedToSeller)):
-				return "returned_to_seller"
+			for _, textIn := range filter.GetFilterTextIn().Value {
+				switch {
+				case textIn == string(model.OfferStatusCodeNew):
+					return "new"
+				case textIn == string(model.OfferStatusCodeSales):
+					return "sales"
+				case textIn == string(model.OfferStatusCodeInOrder):
+					return "order"
+				case textIn == string(model.OfferStatusCodeSold):
+					return "sold"
+				case textIn == string(model.OfferStatusCodeReturnedToSeller):
+					return "returned_to_seller"
+				}
 			}
 		}
 	}
@@ -148,9 +154,32 @@ func getOfferStatusFilterValue(filters *v1.GetListRequest_FilterGroup) string {
 
 func (s server) ListOffersConfig(ctx context.Context, _ *offer_read_service.ListOffersConfigRequest) (*offer_read_service.ListOffersConfigResponse, error) {
 	listOfferStatus, _ := s.root.Repositories.OfferStatusRepository.ListOfferStatus(ctx)
-
 	return &offer_read_service.ListOffersConfigResponse{
 		Data: &v1.GetListConfigResponse{
+			Sort: &v1.GetListConfigResponse_Sort{
+				Fields: []*v1.GetListConfigResponse_Sort_SortField{
+					{
+						Field: "offer.is_new_calculate_date",
+						Label: "Дата получения статуса 'new'",
+					},
+					{
+						Field: "offer.is_sales_calculate_date",
+						Label: "Дата получения статуса 'sales'",
+					},
+					{
+						Field: "offer.is_order_calculate_date",
+						Label: "Дата получения статуса 'order'",
+					},
+					{
+						Field: "offer.is_sold_calculate_date",
+						Label: "Дата получения статуса 'sold'",
+					},
+					{
+						Field: "offer.is_returned_to_seller_calculate_date",
+						Label: "Дата получения статуса 'returned'",
+					},
+				},
+			},
 			Filters: []*v1.GetListConfigResponse_Filter{
 				{
 					Variant: &v1.GetListConfigResponse_Filter_Field_{
@@ -166,6 +195,19 @@ func (s server) ListOffersConfig(ctx context.Context, _ *offer_read_service.List
 											Text: offerStatus.Title,
 										}
 									}),
+								},
+							},
+						},
+					},
+				},
+				{
+					Variant: &v1.GetListConfigResponse_Filter_Field_{
+						Field: &v1.GetListConfigResponse_Filter_Field{
+							Label:     "Идентификатор продавца",
+							FieldName: fieldOfferSellerID,
+							Filters: []*v1.GetListConfigResponse_FieldFilter{
+								{
+									Type: v1.FilterType_FILTER_TYPE_NUMERIC_IN,
 								},
 							},
 						},
